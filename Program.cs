ee63dcd5b8c3a48f2ac46610c6ed36b5fced0f74
cmclark00 <cmclark00@gmail.com>
2022-11-21 12:42:38 -0500

    Fixed weird bug. Now complete.

--- Program.cs
+++ Program.cs
@@ -16,106 +16,119 @@ namespace XBLA_AUTOCSHARP
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
 
-                static void unpackRar()
-                {
-                    string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                    string packedPath = homePath + "\\XBLA\\";
-                    string unpackedPath = homePath + "\\XBLA_Unpacked\\";
-                    string unrarPath = homePath + "\\unRAR.exe";
+            static void unpackRar()
+            {
+                string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string packedPath = homePath + "\\XBLA\\";
+                string unpackedPath = homePath + "\\XBLA_Unpacked\\";
+                string unrarPath = homePath + "\\unRAR.exe";
 
 
-                    var process = Process.Start(unrarPath, " x " + packedPath + "*.rar  " + unpackedPath);
-                    process.WaitForExit();
-                    return;
+                var process = Process.Start(unrarPath, " x " + packedPath + "*.rar  " + unpackedPath);
+                process.WaitForExit();
+
+                return;
 
 
-                }
-
-                static List<string> GetAllFiles(string unpackedPath)
-                {
-
-                    return Directory.GetFiles(unpackedPath, "*", SearchOption.AllDirectories).ToList();
-
-                }
-
-                [DllImport("user32.dll")]
-                static extern bool SetForegroundWindow(IntPtr hWnd);
-                static void unPirs()
-                {
-
-                    string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                    string unpackedPath = homePath + "\\XBLA_Unpacked\\";
-                    string wxPirsPath = homePath + "\\wxPirs.exe";
-                    var files = GetAllFiles(unpackedPath);
-
-
-
-                    foreach (var file in files)
-                    {
-                        Console.WriteLine(file);
-                        var process = Process.Start(wxPirsPath, "\"" + file + "\"");  
-                        Thread.Sleep(2500);
-                        IntPtr hWnd = process.MainWindowHandle;
-                        SetForegroundWindow(hWnd);
-                        SendKeys.SendWait("%{F}");
-                        Thread.Sleep(1000);
-                        SendKeys.SendWait("{ENTER}");
-                        Thread.Sleep(1000);
-                        SendKeys.SendWait("{DOWN}");
-                        Thread.Sleep(1000);
-                        SendKeys.SendWait("{ENTER}");
-                        Thread.Sleep(1000);
-                        SendKeys.SendWait("{ENTER}");
-                        Thread.Sleep(10000);
-                        process.Kill();
-
-                    }
-
-                }
-
-                static void ReNamePirs()
-                {
-                    string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                    string unpackedPath = homePath + "\\XBLA_Unpacked\\";
-                    string renamePath = homePath + "\\XBLA_Unpacked";
-                    List<string> sourceName = Directory.GetFiles(unpackedPath, "default.xex", SearchOption.AllDirectories).ToList();
-
-
-                    for (int i = 0; i < sourceName.Count; i++)
-                    {
-                        DirectoryInfo directoryInfo = new DirectoryInfo(renamePath);
-                        FileInfo fileInfo = directoryInfo.GetFiles("default.xex", SearchOption.AllDirectories).FirstOrDefault();
-
-                        if (fileInfo != null)
-                        {
-                            string newFileName = fileInfo.FullName.Replace(Path.GetFileNameWithoutExtension(fileInfo.Name), fileInfo.Directory.Parent.Parent.Name);
-                            fileInfo.MoveTo(newFileName);
-                        }
-
-                    }
-
-                    return;
-                }
-
-                static void ReNameDirectory()
-                {
-                    string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                    string unpackedPath = homePath + "\\XBLA_Unpacked\\";
-                    string renamePath = homePath + "\\XBLA_Unpacked";
-                    List<string> directoryName = Directory.GetDirectories(unpackedPath, "*", SearchOption.TopDirectoryOnly).ToList();
-
-                    for (int i = 0; i < directoryName.Count; i++)
-                    {
-                        Directory.Move(directoryName[i], directoryName[i] + ".xex");
-                    }
-                    return;
-                }
-
-                unpackRar();
-                unPirs();
-                ReNamePirs();
-                ReNameDirectory();
             }
+
+            static List<string> GetAllFiles(string unpackedPath)
+            {
+
+                return Directory.GetFiles(unpackedPath, "*", SearchOption.AllDirectories).ToList();
+
+            }
+
+            [DllImport("user32.dll")]
+            static extern bool SetForegroundWindow(IntPtr hWnd);
+            static void unPirs()
+            {
+
+                string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string unpackedPath = homePath + "\\XBLA_Unpacked\\";
+                string wxPirsPath = homePath + "\\wxPirs.exe";
+                var files = GetAllFiles(unpackedPath);
+
+
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                    var process = Process.Start(wxPirsPath, "\"" + file + "\"");
+                    Thread.Sleep(5000);
+                    IntPtr hWnd = process.MainWindowHandle;
+                    SetForegroundWindow(hWnd);
+                    SendKeys.SendWait("%{F}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{ENTER}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{DOWN}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{ENTER}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{TAB}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{TAB}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{UP}");
+                    Thread.Sleep(1000);
+                    SendKeys.SendWait("{UP}");
+                    Thread.Sleep(1000); 
+                    SendKeys.SendWait("{ENTER}");
+                    Thread.Sleep(10000);
+
+                    process.Kill();
+                    process.WaitForExit();
+                }
+
+            }
+
+            static void ReNamePirs()
+            {
+                string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string unpackedPath = homePath + "\\XBLA_Unpacked\\";
+                string renamePath = homePath + "\\XBLA_Unpacked";
+                List<string> sourceName = Directory.GetFiles(unpackedPath, "default.xex", SearchOption.AllDirectories).ToList();
+
+
+                for (int i = 0; i < sourceName.Count; i++)
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(renamePath);
+                    FileInfo fileInfo = directoryInfo.GetFiles("default.xex", SearchOption.AllDirectories).FirstOrDefault();
+
+                    if (fileInfo != null)
+                    {
+                        string newFileName = fileInfo.FullName.Replace(Path.GetFileNameWithoutExtension(fileInfo.Name), fileInfo.Directory.Name);
+                        fileInfo.MoveTo(newFileName);
+                    }
+
+                }
+
+                return;
+            }
+
+            static void ReNameDirectory()
+            {
+                string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string unpackedPath = homePath + "\\XBLA_Unpacked\\";
+                string renamePath = homePath + "\\XBLA_Unpacked";
+                List<string> directoryName = Directory.GetDirectories(unpackedPath, "*", SearchOption.TopDirectoryOnly).ToList();
+
+                for (int i = 0; i < directoryName.Count; i++)
+                {
+                    Directory.Move(directoryName[i], directoryName[i] + ".xex");
+                }
+                return;
+            }
+
+            
+
+            unpackRar();
+            unPirs();
+            ReNamePirs();
+            ReNameDirectory();
+           
         }
     }
+}
     
